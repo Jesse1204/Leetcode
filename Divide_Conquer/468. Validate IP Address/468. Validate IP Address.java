e5aab5222cbfdabe5e62b0bb7cac5ac2bdd70874
Jesse1204 <ziheng.cao@nyu.edu>
2020-06-23 23:11:23 -0400

    6/23 Update

---
+++ Divide_Conquer/468. Validate IP Address/468. Validate IP Address.java
@@ -0,0 +1,26 @@
+import java.net.*;
+import java.util.regex.Pattern;;
+
+// Approach 1: Built-in function, not applicable
+class Solution {
+  public String validIPAddress1(String IP) {
+    try {
+      return (InetAddress.getByName(IP) instanceof Inet6Address) ? "IPv6": "IPv4";
+    } catch(Exception e) {}
+    return "Neither";
+  }
+
+// Approach 2: Regex
+  String chunkIPv4 = "([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])";
+  Pattern pattenIPv4 =
+          Pattern.compile("^(" + chunkIPv4 + "\\.){3}" + chunkIPv4 + "$");
+
+  String chunkIPv6 = "([0-9a-fA-F]{1,4})";
+  Pattern pattenIPv6 =
+          Pattern.compile("^(" + chunkIPv6 + "\\:){7}" + chunkIPv6 + "$");
+
+  public String validIPAddress(String IP) {
+    if (pattenIPv4.matcher(IP).matches()) return "IPv4";
+    return (pattenIPv6.matcher(IP).matches()) ? "IPv6" : "Neither";
+  }
+}
--- Divide_Conquer/468. Validate IP Address/README.md
+++ Divide_Conquer/468. Validate IP Address/README.md
@@ -1,10 +1,28 @@
 ### 468. Validate IP Address  
   
-#### 解法：
-
+#### 解法1 Java/Python 内置函数：
   
-时间复杂度： 
+Java和Python内置了验证IP地址的函数，直接调用即可。如果IPv4和IPv6都无法匹配上，直接抛出异常return "Neither". 然而这道题中的要求和真实的IP地址不同，这道题要求每一位不能以0开头，例如01.01.01.01是不行的，然而现实世界中这个IP地址等同于1.1.1.1。
+  
+时间复杂度：O(1)  
 
-空间复杂度：  
+空间复杂度：O(1)  
+  
+#### 解法2 正则表达式：  
+正则表达式需要多加练习。  
+[学习地址](https://github.com/ziishaned/learn-regex/blob/master/translations/README-cn.md)  
+  
+时间复杂度：O(1)  
+
+空间复杂度：O(1)  
+  
+#### 解法3 分治：  
+见下图：  
+  
+  
+时间复杂度：O(n)，知道有多少个点需要parse整个string一次  
+
+空间复杂度：O(1)  
   
 [Leetcode Link](https://leetcode.com/problems/validate-ip-address/)
+[Leetcode Article](https://leetcode.com/articles/validate-ip-address/)
